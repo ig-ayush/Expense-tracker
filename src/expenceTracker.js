9cfcb0c7c1757842d3e9216b716e1382c2a1bad9
ig-ayush <igayushacharya@gmail.com>
2025-02-09 21:51:34 +0530

    Completed the project structure

--- src/expenceTracker.js
+++ src/expenceTracker.js
@@ -2,3 +2,55 @@ function Home(){
     window.location.href = "../index.html";
 };
 
+document.addEventListener("DOMContentLoaded",()=>{
+    document.getElementById('profile').classList.add('load');
+    document.getElementById('totalbal').classList.add('load');
+    document.getElementById('container').classList.add('animate-fadeIn');
+});
+
+// Profile
+let username = document.getElementById('Username');
+let balance = document.getElementById('balance');
+
+username.innerText = localStorage.getItem('Name');
+balance.innerText = localStorage.getItem('Balance');
+
+// Tracker 
+
+const addExpence = document.getElementById('add-expence');
+const list = document.getElementById('list');
+let expenceData = JSON.parse(localStorage.getItem('Expences')) || [];
+
+addExpence.addEventListener('click',function(){
+
+    const expence = document.getElementById('expence-name').value;
+    const amount = document.getElementById('amount').value;
+
+    if(expence != "" && amount != ""){
+        let data = {
+            expence,
+            amount
+        };
+        expenceData.push(data);
+        localStorage.setItem("Expences",JSON.stringify(expenceData));
+        displayExpenses();
+    }else{
+        alert("You did't fill the details");
+    }
+});
+
+const displayExpenses = () =>{
+
+    list.innerHTML = '';
+    expenceData.forEach((expence,index) => {
+        const item = document.createElement('li');
+        const hr = document.createElement('hr');
+        item.innerHTML = `
+        <span><button onclick = deleteExpencce(${index}) class="del"><i class="fa-solid fa-trash"></i></button> &nbsp; ${expence.expence.toUpperCase()}  </span>
+        <span>&#8377; ${expence.amount} </span>`;
+        list.appendChild(item);
+        list.appendChild(hr);
+    });
+};
+
+document.addEventListener('DOMContentLoaded',displayExpenses);
